edfeb2972c89fc3e1b57124e46c83044fb1879dd
Kadir Duran <steadysam32@gmail.com>
2022-11-08 13:39:20 +0300

    Update gaussiandistribution_05_19.py

--- UDACITY/AWS Machine Learning Foundations/05_AWS Machine Learning Foundations_ Introduction to Object Oriented Programming/gaussiandistribution_05_19.py
+++ UDACITY/AWS Machine Learning Foundations/05_AWS Machine Learning Foundations_ Introduction to Object Oriented Programming/gaussiandistribution_05_19.py
@@ -1,6 +1,6 @@
 import math
 import matplotlib.pyplot as plt
-from Generaldistribution import Distribution
+from generaldistribution_05_19 import Distribution
 
 class Gaussian(Distribution):
 	""" Gaussian distribution class for calculating and 
