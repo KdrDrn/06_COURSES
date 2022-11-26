8f2514d87f189f0549fce97b301c3099721e5013
Kadir Duran <steadysam32@gmail.com>
2022-11-26 11:13:30 +0300

    261122

---
+++ UDACITY/AWS Machine Learning Foundations/05_AWS Machine Learning Foundations_ Introduction to Object Oriented Programming/5_exercise_upload_to_pypi/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_v021',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['distributions_v021'],
+      zip_safe=False)
