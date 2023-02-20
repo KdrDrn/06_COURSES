cd0df29b9a984ec2bab5365ede86bb55a45efca8
Kadir DURAN <steadysam32@gmail.com>
2023-02-20 12:01:58 +0300

    Create SchoolDatabase_Student.sql

---
+++ SELF_STUDY/SQL/essentialSQL/SchoolDB/SchoolDatabase_Student.sql
@@ -0,0 +1,43 @@
+----- Problem 1
+-- The schools would like to know which student has the highest GPA. To qualify students, need to have taken two or more courses.
+-- List the Student Name, number of courses taken, and overall GPA.
+
+
+
+
+
+----- Problem 2
+-- In order to plan for some new hires, the school would like to know which course is most popular.
+-- Create a cross tab counting the number of courses taken. The row should be student enrollment year, the columns, course title.
+
+
+
+
+
+----- Problem 3
+-- The facilities manager would like to create a directory of Instructor Offices. 
+-- To help them out, you’re going to provide them a result containing the Building Name, Office Number, and Instructor Name.
+-- The results should be ordered by Building Name and then Office Number.
+
+
+
+
+
+----- Problem 4
+-- There is a fear the Person table, which isn’t normalized, has a data issue.
+-- The column discriminator should either be “Instructor” or “Student” depending on whether HireDate or EnrollmentDate, respectively are filled.
+-- Write a query to check the discriminator colum is correctly based on HireDate and EnrollmentDate. Your query should output the PersonID,
+-- LastName, DatePresent (Hire or Enrollment), Flag. The Flag will be “Discriminator OK” if the data check out, and “Discriminator Bad” if it doesn’t.
+
+
+
+
+
+----- Problem 5
+-- Create course schedules for all student’s onsite courses Include the Student Name, Department Name, Course Title, Location, Days and Time.
+-- Order the schedule by student last name, department name, and course id. 
+
+
+
+
+
